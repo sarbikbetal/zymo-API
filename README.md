0a97374a0ad9bfe1e4a41b93c1d95c44d45d6fdd
Sarbik Betal <sarbikb55@gmail.com>
2020-04-22 00:27:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zymo
+Syncronized coloured screens across your devices, ideal for parties.
---
+++ server.js
@@ -0,0 +1,95 @@
+// Requires
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const helmet = require('helmet');
+const socketio = require('socket.io');
+const NodeCache = require("node-cache");
+const Sentry = require('@sentry/node');
+
+// Inits
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server);
+const roomCache = new NodeCache({
+    stdTTL: 7200
+});
+
+
+// Use helmet middleware
+app.use(helmet());
+
+// Sentry Init
+Sentry.init({
+    dsn: process.env.SENTRY_DSN
+});
+
+// //// Sentry Error reporting ///////
+app.use(Sentry.Handlers.requestHandler());
+
+// Normal route handlers. 
+
+app.get('/room', (req, res) => {
+    let room = req.query.r;
+    let exists = roomCache.has(room);
+    res.json({
+        exists: exists
+    });
+})
+
+// Node-Cache stats
+app.get('/cache-stats', (req, res) => {
+    res.json(roomCache.getStats());
+})
+
+app.use(Sentry.Handlers.errorHandler());
+
+
+// Check for production env and upgrade to https
+if (process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS({
+        trustProtoHeader: true
+    }));
+}
+
+// Socket io server handler
+io.on('connection', (socket) => {
+
+    // TODO: maintain a list of hosted rooms
+    // user hosts a room
+    let isHost = false;
+    let hostID = null;
+    socket.on('host-room', (reply) => {
+        let roomID = Date.now().toString(36);
+        socket.leave(socket.id);
+        socket.join(roomID);
+        hostID = roomID;
+        isHost = true;
+        roomCache.set(roomID, true);
+        reply(roomID);
+    });
+
+    // user joins a room
+    socket.on('join-room', (roomID, reply) => {
+        socket.join(roomID);
+        reply(`${socket.id} joined ${roomID}`);
+    });
+
+    // Actual color message
+    socket.on('color', (color) => {
+        for (key in socket.rooms) {
+            io.to(key).emit('color', color);
+        }
+    });
+
+    // TODO: remove from the list of hosted rooms
+    socket.on('disconnect', () => {
+        if (isHost)
+            roomCache.del(hostID);
+    })
+})
+
+const PORT = process.env.PORT || 5500;
+server.listen(PORT, () => {
+    console.log(`Server started at port:${PORT}`);
+});
